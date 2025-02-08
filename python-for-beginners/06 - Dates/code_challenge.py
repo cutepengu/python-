6600fa76f9b67f5d174cf06c5b09a9c4678e4159
ErinLee <erin.lee22@education.nsw.gov.au>
2025-02-08 21:42:21 +1100

    08/02/2025
    sunday pt2-
    up to 13. Functions

--- python-for-beginners/06 - Dates/code_challenge.py
+++ python-for-beginners/06 - Dates/code_challenge.py
@@ -1,4 +1,15 @@
 # print today's date
+from datetime import datetime, timedelta
+current_date = datetime.now()
+print(current_date)
 # print yesterday's date
+one_day = timedelta(days=1)
+yesterday = today - one_day
+print('yesterday was: ' + str(yesterday))
 # ask a user to enter a date
+enter_date = input('enter a date: (dd/mm/yyyy) ')
+enter_date = datetime.strptime(enter_date, '%d/%m/%Y')
 # print the date one week from the date entered
+one_week = timedelta(weeks=1)
+last_week = today + one_week
+print('next week is: ' + str(last_week))
--- python-for-beginners/09 - Handling multiple conditions/code_challenge.py
+++ python-for-beginners/09 - Handling multiple conditions/code_challenge.py
@@ -1,11 +1,26 @@
 # Ask a user their name
+name = input('what is your name? ')
 # If their first name starts with A or B 
 # tell them they go to room AB
+first_letter = name[0:1]
+if first_letter.upper() in ('A','B'):
+    room = 'AB'
 # IF their first name starts with C
 # tell them to go to room CD
+elif first_letter.upper() == 'C':
+    room = 'CD'
 # If their first name starts with another letter, ask for their last name
+else:
+    last_name = input('what is your last name? ')
+    last_name_first_letter = last_name[0:1]
 # IF their last name starts with Z, tell them to go to room Z
+    if last_name_first_letter == 'Z':
+        room = 'Z'
 # if their last name starts with any other letter, tell them to go to room OTHER
+    else:
+        room = 'OTHER'
+
+print('go to room' + room)
 # When you are done
 # Anna should be in room AB
 # Bob should be in room AB
